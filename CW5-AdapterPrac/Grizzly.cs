beaeb2215e8fcfdfe9b05c7ffdc00decad9a1f28
Kenneth Wallace <jhekwall@ut.utm.edu>
2023-02-09 14:18:48 -0600

    Implementation

---
+++ CW5-AdapterPrac/Grizzly.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5_AdapterPrac
+{
+    internal class Grizzly : IBear
+    {
+        public void maul()
+        {
+            Console.WriteLine("Grizzly uses maul!\n");
+        }
+
+        public void hibernate()
+        {
+            Console.WriteLine("Grizzly uses hibernate!\n");
+        }
+    }
+}
---
+++ CW5-AdapterPrac/TeddyBear.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5_AdapterPrac
+{
+    internal class TeddyBear : IToyBear
+    {
+        public void hug()
+        {
+            Console.WriteLine("Teddybear uses hug!\n");
+        }
+    }
+}
