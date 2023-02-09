31b532b0f12f5a709ec9aa9facd04f073f3804a6
Kenneth Wallace <jhekwall@ut.utm.edu>
2023-02-09 13:40:12 -0600

    Add project files.

---
+++ CW5-AdapterPrac/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW5_AdapterPrac
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
